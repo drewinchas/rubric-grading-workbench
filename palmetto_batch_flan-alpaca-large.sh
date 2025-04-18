697016e661f8532b7015c50d7a76cda56012f5c9
drewinchas <adyork@clemson.edu>
2025-04-18 14:42:02 -0400

    Multiple updates for CPSC 8470 Phase 3 paper.

---
+++ palmetto_batch_flan-alpaca-large.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Submit this script with: sbatch FILENAME
+
+#SBATCH --ntasks 1           # number of tasks
+#SBATCH --cpus-per-task 2    # number of cpu cores per task
+#SBATCH --time 16:00:00      # walltime
+#SBATCH --mem 16gb           # amount of memory per CPU core (Memory per Task / Cores per Task)
+#SBATCH --nodes 1            # number of nodes
+#SBATCH --gpus-per-task v100s:2 # gpu model and amount requested
+#SBATCH --job-name "rubric-grading-workbench-flan-alpaca-large" # job name
+# Created with the RCD Docs Job Builder
+#
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+source .venv/bin/activate
+export BATCH_SIZE=10
+export GPU_DEVICE=0
+export NUM_WORKERS=2
+sh walkthrough-dl20_phases2thru4_flan-alpaca-large.sh 2>&1 |tee palmetto_batch_flan-alpaca-large_run.log
--- palmetto_prep.bash
+++ palmetto_prep.bash
@@ -8,29 +8,46 @@
 # Build standalone version of sqlite3
 #
 
-wget https://www.sqlite.org/2025/sqlite-autoconf-3490100.tar.gz
-tar -zxvf sqlite-autoconf-3490100.tar.gz
-cd sqlite-autoconf-3490100/
-./configure
-make
-cd ..
+#wget https://www.sqlite.org/2025/sqlite-autoconf-3490100.tar.gz
+#tar -zxvf sqlite-autoconf-3490100.tar.gz
+#cd sqlite-autoconf-3490100/
+#./configure
+#make
+#cd ..
+
+#
+# Build standalone version of libffi
+# 
+#wget https://github.com/libffi/libffi/releases/download/v3.4.8/libffi-3.4.8.tar.gz
+#tar -zxvf libffi-3.4.8.tar.gz
+#cd libffi-3.4.8/
+#./configure
+#make
+#cd ..
 
 #
 # Build new version of Python
 #
 
-wget https://www.python.org/ftp/python/3.13.2/Python-3.13.2.tgz
-tar -zxvf Python-3.13.2.tgz
-cd Python-3.13.2/
-LIBSQLITE3_CFLAGS="-I../sqlite-autoconf-3490100" LIBSQLITE3_LIBS="-L../sqlite-autoconf-3490100" ./configure --enable-loadable-sqlite-extensions --enable-optimizations
-make
-cd ..
+#wget https://www.python.org/ftp/python/3.13.2/Python-3.13.2.tgz
+#tar -zxvf Python-3.13.2.tgz
+#cd Python-3.13.2/
+#LIBFFI_CFLAGS="-I../libffi-3.4.8/x86_64-pc-linux-gnu/include/" LIBFFI_LIBS="-L../libffi-3.4.8/x86_64-pc-linux-gnu" LIBSQLITE3_CFLAGS="-I../sqlite-autoconf-3490100" LIBSQLITE3_LIBS="-L../sqlite-autoconf-3490100" ./configure --enable-loadable-sqlite-extensions --enable-optimizations
+#make
+#cd ..
+
+#
+# Install Miniconda (Accept defaults)
+#
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh
+
 
 #
 # Create virtual environment (venv)
 #
 
-Python-3.13.2/python -m venv .venv
+../miniconda3/bin/python -m venv .venv
 source .venv/bin/activate
 pip install setuptools
 
@@ -38,13 +55,13 @@ pip install setuptools
 # Build pysqlite3 module (local sqlite3 wrapper)
 #
 
-git clone https://github.com/coleifer/pysqlite3
-cd pysqlite3/
-cp ../sqlite-autoconf-3490100/sqlite3.c .
-cp ../sqlite-autoconf-3490100/sqlite3.h .
-python setup.py build_static
-python setup.py install
-cd ..
+#git clone https://github.com/coleifer/pysqlite3
+#cd pysqlite3/
+#cp ../sqlite-autoconf-3490100/sqlite3.c .
+#cp ../sqlite-autoconf-3490100/sqlite3.h .
+#python setup.py build_static
+#python setup.py install
+#cd ..
 
 #
 # Install dependencies
@@ -59,7 +76,7 @@ poetry install
 # Patch NLTK to use pysqlite3 (local sqlite3 wrapper)
 #
 
-patch .venv/lib/python3.13/site-packages/nltk/corpus/reader/panlex_lite.py < palmetto.patch
+#patch .venv/lib/python3.13/site-packages/nltk/corpus/reader/panlex_lite.py < palmetto.patch
 
 #
 # Get DL20 dataset for analysis
